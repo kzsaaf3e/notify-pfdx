ab2eaf57d04c2cfed0efb36d5775fba6a619f83c
kzsaaf3e <157870730+kzsaaf3e@users.noreply.github.com>
2024-02-21 00:43:17 +0700

    Add files via upload

---
+++ pinkfoxdx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Before sending the curl request, make sure to set the bot token:
+export TELEGRAM_BOT_TOKEN=6788930639:AAHpp3siVn8wnWp3SGOM_uC2EDFaXWjyE6I
+
+# Sending the curl request: 
+curl -X POST -H 'Content-Type: application/json' \
+  -d '{"message_thread_id": "287", "chat_id": "-1001805014416_287", "text":"Test", "reply_markup": {"inline_keyboard": [[{"text":"LaResistencia.co", "url": "http://laresistencia.co"}]]} }' \
+  https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
