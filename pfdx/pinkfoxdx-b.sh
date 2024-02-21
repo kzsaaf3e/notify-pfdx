d79ef8ad20bbbbd824cffbe2625f006ab452f238
kzsaaf3e <157870730+kzsaaf3e@users.noreply.github.com>
2024-02-21 07:09:20 +0700

    Update pinkfoxdx-b.sh using ${{ github.run_id }} to create link
    Test

--- pfdx/pinkfoxdx-b.sh
+++ pfdx/pinkfoxdx-b.sh
@@ -5,5 +5,5 @@ export TELEGRAM_BOT_TOKEN=6788930639:AAHpp3siVn8wnWp3SGOM_uC2EDFaXWjyE6I
 
 # Sending the curl request: 
 curl -X POST -H 'Content-Type: application/json' \
-  -d '{"message_thread_id": "287", "chat_id": "-1001805014416_287", "text":"Test", "reply_markup": {"inline_keyboard": [[{"text":"LaResistencia.co", "url": "http://laresistencia.co"}]]} }' \
+  -d '{"message_thread_id": "287", "chat_id": "-1001805014416_287", "text":"Test", "reply_markup": {"inline_keyboard": [[{"text":"Gaskann", "url": "https://github.com/Massatrio16/Tes/releases/tag/${{ github.run_id }}"}]]} }' \
   https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
